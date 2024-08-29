843a10b66e94efd21d3cdf0c97f135343599b869
gabrielvictor733 <143092384+gabrielvictor733@users.noreply.github.com>
2024-08-29 15:13:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,85 @@
-const pesquisar = document.getElementById("pesquisa")
-const listar = document.getElementById("lista")
+const pesquisar = document.getElementById("pesquisa");
+const listar = document.getElementById("lista");
+
+
+const inputs = {
+    data: {
+        type: 'date',
+        id: 'data',
+        placeholder: 'Data'
+    },
+    hora: {
+        type: 'time',
+        id: 'hora',
+        placeholder: 'Hora'
+    }
+};
+
+
+function criarInputs() {
+    const containerPesquisa = document.querySelector('.pesquisa');
+
+    for (let key in inputs) {
+        let input = document.createElement('input');
+        let config = inputs[key];
+
+
+        input.type = config.type;
+        input.id = config.id;
+        input.placeholder = config.placeholder;
+
+
+        containerPesquisa.appendChild(input);
+    }
+}
+
+
+criarInputs();
 
 function AdicionarTarefa() {
-    if (pesquisar.value === '') {
+    const inputData = document.getElementById("data");
+    const inputHora = document.getElementById("hora");
+    const tarefa = pesquisar.value.trim();
+    const data = formatarData(inputData.value);
+    const hora = inputHora.value;
+
+    if (tarefa === '') {
         alert("Digite uma tarefa");
-    }
-    else {
+    } else {
         let li = document.createElement("li");
-        li.innerHTML = pesquisar.value;
+
+
+        li.innerHTML = `${tarefa}`;
+        if (data || hora) {
+            li.innerHTML += ` - <small>${data ? data : ''} ${hora ? hora : ''}</small>`;
+        }
+
         listar.appendChild(li);
+
         let span = document.createElement("span");
-        span.innerHTML = "\u00d7"
+        span.innerHTML = "\u00d7";
         li.appendChild(span);
     }
+
     pesquisar.value = "";
+    inputData.value = "";
+    inputHora.value = "";
+
     SalvarTarefas();
+}
+
+
+function formatarData(data) {
+    if (!data) return '';
+    const [ano, mes, dia] = data.split('-');
+    return `${dia}/${mes}/${ano}`;
 }
 
 listar.addEventListener("click", function (e) {
     if (e.target.tagName === "LI") {
         e.target.classList.toggle("checked");
         SalvarTarefas();
-    }
-    else if (e.target.tagName === "SPAN") {
+    } else if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
         SalvarTarefas();
     }
@@ -33,6 +90,14 @@ function SalvarTarefas() {
 }
 
 function MostrarTarefas() {
-    listar.innerHTML = localStorage.getItem("tarefa");
+    listar.innerHTML = localStorage.getItem("tarefa") || '';
 }
+
 MostrarTarefas();
+
+
+pesquisar.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+        AdicionarTarefa();
+    }
+});
